9a48b3cf0ab7a6d9257fecb1cfd3ef5eb09a0b62
jorhelp <2373911050@qq.com>
2021-05-12 00:19:11 +0800

    about

--- static/js/base.js
+++ static/js/base.js
@@ -51,18 +51,6 @@ function foorter_motion_font() {
     binft(document.getElementById('binft'));
 }
 
-// 验证码刷新
-$('img.captcha').click(function() {
-    $.getJSON(
-        '/captcha/refresh/',
-        function(json) {
-        // This should update your captcha image src and captcha hidden input
-        console.log(json); $("img.captcha").attr("src",json.image_url);
-        $("#id_captcha_0").val(json.key);
-    });
-    return false;
-});
-
 
 // 返回顶部
 function back_to_top(){
